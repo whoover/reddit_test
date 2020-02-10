333188160b9dfe4d3cefa7edb5655e3e9a69655c
whoover <whoover.89@gmail.com>
2020-02-10 22:54:45 +0200

    [add] added initial project

---
+++ Reddit_test/Reddit_test/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Reddit_test
+//
+//  Created by Artem Belenkov on 10.02.2020.
+//  Copyright © 2020 Artem Belenkov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
